deb97f5822e4787f6679c2e2ba312c9cafd632c0
Tyson Hamilton <thamilton@mdialog.com>
2014-04-25 09:48:19 -0400

    * Correct TODO messages * Update README.md * Rename core project to examples

---
+++ examples/src/main/scala/Examples.scala
@@ -0,0 +1,21 @@
+@TODO("2013/04/10: This annotation will abort compilation if not done by 2013/04/10") 
+object TodoExample {
+  
+  @TODO("This annotation always generates a warning")
+  def apply() {
+    TODO("2013/04/10: This expression will abort compilation if not done by 2013/04/10")
+    println("todo")
+  }
+}
+
+
+
+@FIXME("2013/04/10: This annotation will abort compilation if not fixed by 2013/04/10") 
+object FixmeExample {
+
+  @FIXME("This annotation always generates a warning")
+  def apply() {
+    FIXME("2013/04/10: This expression will abort compilation if not fixed by 2013/04/10")
+    println("fixme")
+  }
+}
